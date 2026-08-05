837b381375a2ad1811cd6469a5465e418ed1a4a1
iishida-AG <306423794+iishida-AG@users.noreply.github.com>
2026-08-05 16:14:55 +0900

    chore: configure Firebase project

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyAvu3oX0oE8F2_NcZPRVBZntSdYJU20lkQ',
+  authDomain: 'ishida-ai-tool-dev.firebaseapp.com',
+  projectId: 'ishida-ai-tool-dev',
+  storageBucket: 'ishida-ai-tool-dev.firebasestorage.app',
+  messagingSenderId: '942724680628',
+  appId: '1:942724680628:web:d9cde494314d89ad3e00e1'
 };
